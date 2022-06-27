192caf0c43d984b702832e2c2f323bcac3edd9ea
unknown <laralourenco5966@gmail.com>
2022-06-27 14:42:19 -0300

    Estilo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {FiSearch} from 'react-icons/fi';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite o CEP que deseja verificar"
+        />
+
+        <button className='buttonSearch'>
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+        </div>
+
+        <main className='main'>
+          <h2>CEP: TESTE</h2>
+          <span>Rua TESTE</span>
+          <span>Complemento</span>
+          <span>Bairro</span>
+          <span>Cidade - Estado</span>
+        </main>
+
+      
     </div>
   );
 }
